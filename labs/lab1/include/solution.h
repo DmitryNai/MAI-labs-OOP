3ad25e4a16b5e1ee2e99d72af46657a24bbb532f
DmitryNai <d.naipak@mail.ru>
2024-09-15 13:36:07 +0300

    Lab1 solution

---
+++ labs/lab1/include/solution.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include "../include/solution.cpp"
+
+bool isPalindrom(const std::string& word);
---
+++ labs/lab1/src/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/solution.h"
+#include <iostream>
+
+int main() {
+   std::string s;
+   std::getline(std::cin, s);
+   return 0;
+}
